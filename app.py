63220dbbf961c0918ec4aa67630c21de61f8f47a
Cocoflosbach <84343628+Cocoflosbach@users.noreply.github.com>
2024-03-10 20:28:35 +0100

    Update in-memory data list

--- app.py
+++ app.py
@@ -1,39 +1,41 @@
+"""API for Children's online store application"""
+
 from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-@app.router('/api/data', methods=['GET'])
-def get_data():
-    """Function to get and return list of data"""
-    data = [
+products = [
         {
             "Name": "Baby T-shirt",
             "Color": "Green",
             "Price": "N2000",
-            "Description": "Green Baby T-shirt for babies from newborn to 24 months"
+            "Description": "Green Baby T-shirt for babies from newborn to 24 months",
+            "product_id": 1
         },
         {
             "Name": "Baby Jumper",
             "Color": "Pink",
             "Price": "N2000",
-            "Description": "Pink Baby Jumper for babies from newborn to 24 months"
+            "Description": "Pink Baby Jumper for babies from newborn to 24 months",
+            "product_id": 2
         },
         {
             "Name": "Baby Bottle",
             "Color": "Blue",
             "Price": "N5000",
-            "Description": "Green Baby Bottle for babies from newborn to 24 months"
+            "Description": "Green Baby Bottle for babies from newborn to 24 months",
+            "product_id": 3
         },
         {
             "Name": "Baby Teething Ring",
             "Color": "Nude",
             "Price": "N9000",
-            "Description": "Nude Baby Teething Ring for babies from newborn to 24 months"
+            "Description": "Nude Baby Teething Ring for babies from newborn to 24 months",
+            "product_id": 4
         },
     ]
-    return jsonify(data)
 
 @app.route('/products', methods=['GET'])
 def get_products():
